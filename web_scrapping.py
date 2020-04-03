1b08e889aec4150cad13febb917d13d1a958ad19
cacoool <denis.corbin@hotmail.com>
2020-04-03 15:27:08 -0400

    Gestion de nouvelles erreurs

--- web_scrapping.py
+++ web_scrapping.py
@@ -15,9 +15,7 @@ import pandas as pd  # type: ignore
 import requests
 
 def eprint(message: str):
-    '''
-    Fonction pure: Writes message to std:err
-    '''
+    """ Writes message to std:err """
     print(message, file=sys.stderr)
 
 
@@ -54,9 +52,6 @@ def web_scrapping(in_arg: str, type_arg: str, crawl_arg: str, i_arg: int) -> Non
                 for name, value in attrs:
                     if is_href(name):
                         # Une fois trouvé, le lien est nettoyé afin d'éliminer les faux positifs
-                        # if value.find(';') != -1:
-                        #     continue
-                        # else:
                         value = value.replace("\\", '')
                         value = value.replace("\"", '')
                         value = value.replace("'", '')
@@ -72,14 +67,16 @@ def web_scrapping(in_arg: str, type_arg: str, crawl_arg: str, i_arg: int) -> Non
     to_crawl = True
 
     if type_arg.endswith('h'):
+        # Lecture du fichier
         file = codecs.open(in_arg, "r", "utf-8")
         try:
             html = str(file.read())
             file.close()
         except:
             eprint("Cannot open input file %s" % in_arg)
-
+        # Extraction des liens
         links = regex(html)
+        # Les liens sont considérés externes car on n'a pas le domaine
         for link in links:
             if link not in outlinks and link not in visited.keys():
                 outlinks.append(link)
@@ -101,56 +98,61 @@ def web_scrapping(in_arg: str, type_arg: str, crawl_arg: str, i_arg: int) -> Non
                 eprint("No response from %s. Cannot obtain status code" % url)
                 visited[link] = 0
             # Si le lien existe:
-            if req.status_code == 200:
-                # On parse le html et on applique notre regex pour trouvez tous les liens
-                parser_local = MyHTMLParser()
-                try:
-                    file = urllib.request.urlopen(link)
-                    html = str(file.read())
-                    file.close()
-                except:
-                    eprint("Unable to read html code from %s" % link)
-                links = []
-                links = regex(html)
-                parser_local.feed(html)
+            try:
+                req
+            except NameError:
+                eprint("Main domain is unreachable, are you sure it's online?")
+            else:
+                if req.status_code == 200:
+                    # On parse le html et on applique notre regex pour trouvez tous les liens
+                    parser_local = MyHTMLParser()
+                    try:
+                        file = urllib.request.urlopen(link)
+                        html = str(file.read())
+                        file.close()
+                    except:
+                        eprint("Unable to read html code from %s" % link)
+                    links = []
+                    links = regex(html)
+                    parser_local.feed(html)
 
-                for link in links:
-                    parsed_link = urlparse(link)
-                    loc = parsed_link.netloc
-                    joined_url = urljoin(in_arg, link)
-                    # Si la loc est vide:
-                    if loc == '':
-                        # Vérifier les doublons
-                        if joined_url not in to_visit and joined_url not in visited.keys():
-                            to_visit.append(joined_url)
+                    for link in links:
+                        parsed_link = urlparse(link)
+                        loc = parsed_link.netloc
+                        joined_url = urljoin(in_arg, link)
+                        # Si la loc est vide:
+                        if loc == '':
+                            # Vérifier les doublons
+                            if joined_url not in to_visit and joined_url not in visited.keys():
+                                to_visit.append(joined_url)
 
-                    # Si la loc est égale à la base:
-                    elif loc == base:
-                        # Vérifier les doublons
-                        if link not in to_visit and link not in visited.keys():
-                            to_visit.append(link)
+                        # Si la loc est égale à la base:
+                        elif loc == base:
+                            # Vérifier les doublons
+                            if link not in to_visit and link not in visited.keys():
+                                to_visit.append(link)
 
-                    # Si la loc est différente, c'est donc un lien externe:
-                    else:
-                        # Vérifier les doublons
-                        if link not in outlinks and link not in visited.keys():
-                            outlinks.append(link)
+                        # Si la loc est différente, c'est donc un lien externe:
+                        else:
+                            # Vérifier les doublons
+                            if link not in outlinks and link not in visited.keys():
+                                outlinks.append(link)
 
-                if crawl_arg.endswith('False'):
-                    to_crawl = False
-                    while to_visit:
-                        # On regarde un lien à la fois
-                        link = to_visit.pop()
-                        print(link)
-                        # On essaye d'y accéder dans un délais inférieur de 1 secondes
-                        try:
-                            req = requests.get(link, timeout=2)
-                            visited[link] = req.status_code
-                        # Si on n'y arrive pas, on considère le lien mort
-                        except:
-                            eprint("No response from %s. Cannot obtain status code" % link)
-                            deadlinks.append(link)
-                            visited[link] = 0
+                    if crawl_arg.endswith('False'):
+                        to_crawl = False
+                        while to_visit:
+                            # On regarde un lien à la fois
+                            link = to_visit.pop()
+                            print(link)
+                            # On essaye d'y accéder dans un délais inférieur de 1 secondes
+                            try:
+                                req = requests.get(link, timeout=2)
+                                visited[link] = req.status_code
+                            # Si on n'y arrive pas, on considère le lien mort
+                            except:
+                                eprint("No response from %s. Cannot obtain status code" % link)
+                                deadlinks.append(link)
+                                visited[link] = 0
 
     # On vérifie le status de tous les liens externes afin de trouver ceux qui ne sont pas morts
     print("Checking status of links")
@@ -192,11 +194,6 @@ if __name__ == '__main__':
     print(STDOUT.getvalue())
     print(STDERR.getvalue())
 
-    # Logging
-    import sys
-    import logging
-    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-
     # Parser
     PARSER = argparse.ArgumentParser(description='WebCrawler')
 
@@ -211,35 +208,26 @@ if __name__ == '__main__':
                              "Crawling arg be ignored for local HTML files."
                              "Specify in a list fashion. Not needed for files")
 
-    # OPT = PARSER.parse_args()
-    # OPT = PARSER.parse_args([
-    #     '--IN', 'file.txt',
-    #     '--TYPE', 'f'
-    #     #  '--CRAWL', 'True', 'False'
-    # ])
-
-    OPT = PARSER.parse_args([
-        '--IN', 'tal.html', 'http://localhost:3000/',
-        '--TYPE', 'h', 'u',
-        '--CRAWL', 'False', 'False'
-    ])
+    OPT = PARSER.parse_args()
 
     # URLs et fichiers htlm sont dans un fichier:
     if OPT.TYPE == ['f']:
-        assert len(OPT.IN) == len(OPT.TYPE), \
-            "Input arguments must have the same length"
-        TO_PARSE = update_args(OPT)
-        for i in range(len(TO_PARSE)):
-            OPT = PARSER.parse_args([
-                '--IN', TO_PARSE[i][0],
-                '--TYPE', TO_PARSE[i][1],
-                '--CRAWL', TO_PARSE[i][2]
-            ])
-            assert len(OPT.IN) == len(OPT.TYPE) == len(OPT.CRAWL), \
-                "Input arguments must have the same length"
-            web_scrapping(OPT.IN[0], OPT.TYPE[0], OPT.CRAWL[0], i)
+        if not len(OPT.IN) == len(OPT.TYPE):
+            eprint("Input arguments must have the same length")
+        else:
+            TO_PARSE = update_args(OPT)
+            for i in range(len(TO_PARSE)):
+                OPT = PARSER.parse_args([
+                    '--IN', TO_PARSE[i][0],
+                    '--TYPE', TO_PARSE[i][1],
+                    '--CRAWL', TO_PARSE[i][2]
+                ])
+                assert len(OPT.IN) == len(OPT.TYPE) == len(OPT.CRAWL), \
+                    "Input arguments must have the same length"
+                web_scrapping(OPT.IN[0], OPT.TYPE[0], OPT.CRAWL[0], i)
     else:
-        assert len(OPT.IN) == len(OPT.TYPE) == len(OPT.CRAWL), \
-            "Input arguments must have the same length"
-        for i in range(len(OPT.IN)):
-            web_scrapping(OPT.IN[i], OPT.TYPE[i], OPT.CRAWL[i], i)
+        if not len(OPT.IN) == len(OPT.TYPE):
+            eprint("Input arguments must have the same length")
+        else:
+            for i in range(len(OPT.IN)):
+                web_scrapping(OPT.IN[i], OPT.TYPE[i], OPT.CRAWL[i], i)
